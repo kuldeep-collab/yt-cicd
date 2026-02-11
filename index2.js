fac872ece4b6784c6204444bd2e31107995ae50a
Kuldeep Singh <kuldeepchaudhary534@gmail.com>
2026-02-11 23:03:28 +0530

    added new file

---
+++ index2.js
@@ -0,0 +1,10 @@
+
+<html>
+<head>
+    <title>Kuldeep Singh</title>
+</head>
+<body style="text-align:center; font-family:Arial;">
+    <h1>Kuldeep Singh</h1>
+    <p>Welcome to my page!</p>
+</body>
+</html>
